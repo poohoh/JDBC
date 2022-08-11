add746f50f52200d7951976ef54e6f329ccb3c76
poohoh <kjh980328@gmail.com>
2022-08-11 21:58:41 +0900

    commit -m "NoticeConsole and Creating View"

--- src/com/newlecture/app/service/NoticeService.java
+++ src/com/newlecture/app/service/NoticeService.java
@@ -13,13 +13,15 @@ public class NoticeService {
     private String pwd = "1234";
     private String driver = "oracle.jdbc.driver.OracleDriver";
 
-    public List<Notice> getList() throws ClassNotFoundException, SQLException {
-        String sql = "SELECT * FROM NOTICE";
+    public List<Notice> getList(int page) throws ClassNotFoundException, SQLException {
+        String sql = "SELECT * FROM NOTICE_VIEW WHERE NUM BETWEEN ? AND ?";
 
         Class.forName(driver);
         Connection con = DriverManager.getConnection(url, uid, pwd);
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sql);
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, (page-1)*10+1);
+        st.setInt(2, page*10);
+        ResultSet rs = st.executeQuery();
 
         List<Notice> list = new ArrayList<Notice>();
 
